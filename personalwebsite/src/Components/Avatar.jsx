35c1524a523f402700b1de6c1a1d0a8b2a13309c
Musab Alsayed <musab20151@gmail.com>
2024-07-31 07:19:28 +0300

    add some

--- personalwebsite/src/Components/Avatar.jsx
+++ personalwebsite/src/Components/Avatar.jsx
@@ -1,14 +1,16 @@
 import React from "react";
-import MyPic from "/src/assets/MyPic.jpeg"
+import MyPic from "/src/assets/MyPic.jpeg";
 
 function Avatar() {
   return (
     <div className="flex justify-center items-center">
-      <img
-        className="border-4 border-Green rounded-full h-28 mt-5 mb-5"
-        src={MyPic}
-        alt="avatar"
-      />
+      <div className="p-1 bg-gradient-to-tr from-Green to-Blue rounded-full mt-5 mb-5">
+          <img
+            className="rounded-full h-32 w-32 "
+            src={MyPic}
+            alt="avatar"
+          />
+      </div>
     </div>
   );
 }
--- personalwebsite/tailwind.config.js
+++ personalwebsite/tailwind.config.js
@@ -1,24 +1,24 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    './src/**/*.{js,jsx,ts,tsx}',
-    'node_modules/flowbite-react/lib/esm/**/*.js',
+    "./src/**/*.{js,jsx,ts,tsx}",
+    "node_modules/flowbite-react/lib/esm/**/*.js",
   ],
   theme: {
     extend: {
-
-    },colors:{
-        background:"#272727",
-        Green:"#3BF686",
-        Blue:"#4CA9FF",
-        FooterColor:"#232323",
-        White:"#FBFBFB",
-        BoldWhite:"#D6D6D6",
-        Card:"#323443"
+      colors: {
+        background: "#272727",
+        Green: "#3BF686",
+        Blue: "#4CA9FF",
+        FooterColor: "#232323",
+        White: "#FBFBFB",
+        BoldWhite: "#D6D6D6",
+        Card: "#323443",
+      },
+      translate: {
+        "-0.25em": "-0.25em",
+      },
     },
   },
-  plugins: [
-    require('daisyui'),
-  ],
-}
-
+  plugins: [require("daisyui")],
+};
